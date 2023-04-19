dc5378befcba69adf431a35609ad495eafe043df
DiegoCubides <juand.cubidesor@ecci.edu.co>
2023-04-19 17:45:33 -0500

    prueba

--- R/wall_distance.R
+++ R/wall_distance.R
@@ -4,5 +4,14 @@ normalise <- function(x){
 }
 
 folder <-  dirname(rstudioapi::getSourceEditorContext()$path )
-parentFolder <- dirname(folder)
-Social_Network_Ads <- read_csv(paste0(parentFolder ,"/R/dataset_wall_distance.csv"))
+wall.distance <-read_csv(paste0(folder,"/dataset_wall_distance.csv"))
+
+head(wall.distance)
+
+wall.distance$`DISTANCE(cm)` <-
+  as.factor(wall.distance$`DISTANCE(cm)`)
+
+plot(wall.distance[1:2],pch =21
+     ,bg= c("red","green")[unclass(wall.distance$`DISTANCE(cm)`)])
+
+prop.table(table(wall.distance$`DISTANCE(cm)`))
