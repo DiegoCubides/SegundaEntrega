9eca3bfe5353bc40fcc41b5e76109129d9feb47f
larrycastro <larrya.castror@ecci.edu.co>
2023-04-20 17:39:15 -0500

    larry

--- R/wall_distance.R
+++ R/wall_distance.R
@@ -2,12 +2,15 @@ library(tidyverse)
 normalise <- function(x){
   return((x-min(x))/(max(x)-min(x)))
 }
-
 folder <-  dirname(rstudioapi::getSourceEditorContext()$path )
 wall.distance <-read_csv(paste0(folder,"/dataset_wall_distance.csv"))
 
+## Exploratory Data Analysis
+
 head(wall.distance)
+
 summary(wall.distance)
+
 hist(wall.distance$INFRARED,breaks = 110)
 hist(wall.distance$ULTRASONIC,breaks = 110)
 
@@ -15,11 +18,10 @@ wall.distance$`DISTANCE(cm)` <-as.factor(wall.distance$`DISTANCE(cm)`)
 pairs(wall.distance[c("INFRARED","ULTRASONIC")]
       ,pch=25,bg=c("green","blue3","gray","yellow","green3","pink","brown","black","red","orange")[unclass(wall.distance$`DISTANCE(cm)`)])
 
-
-
 prop.table(table(wall.distance$`DISTANCE(cm)`))
 
-##regresion lineal para sensor infrarojo
+
+##Linear Regression For Infrared Sensor
 x <- wall.distance$INFRARED
 y <- wall.distance$`DISTANCE(cm)`
 b=cov(x,y)/var(x)
@@ -27,15 +29,9 @@ a=mean(y)-b*mean(x)
 a+b*121
 
 
-##regresion lineal para sensor ultrasonico
+##Linear Regression For Ultrasonic Sensor
 x <- wall.distance$ULTRASONIC
 y <- wall.distance$`DISTANCE(cm)`
 b=cov(x,y)/var(x)
 a=mean(y)-b*mean(x)
 a+b*126
-
-
-
-
-
-
