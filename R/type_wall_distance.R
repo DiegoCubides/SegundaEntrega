2d854c1b3ff2f4e1201fd9423ead10aa26a3e884
DiegoCubides <juand.cubidesor@ecci.edu.co>
2023-04-20 12:27:40 -0500

    Modelo_final

--- R/type_wall_distance.R
+++ R/type_wall_distance.R
@@ -6,20 +6,48 @@ type.wall.distance <-read_csv(paste0(folder,"/dataset_type_wall_distance.csv"))
 #Exploratory Data Analysis
 head(type.wall.distance)
 
-type.wall.distance$`DISTANCE(cm)` <-as.factor(type.wall.distance$`DISTANCE(cm)`)
+type.wall.distance$TYPE <-as.factor(type.wall.distance$TYPE)
 
 prop.table(table(type.wall.distance$`DISTANCE(cm)`))
 
 plot(type.wall.distance[1:2],pch =21
-     ,bg= c("red","green")[unclass(type.wall.distance$`DISTANCE(cm)`)])
+     ,bg= c("red","green")[unclass(type.wall.distance$TYPE)])
 hist(type.wall.distance$INFRARED,breaks = 50)
 hist(type.wall.distance$ULTRASONIC,breaks = 50)
 
-dummy<-dummyVars(" ~ TYPE",data = type.wall.distance)
+dummy <- dummyVars(" ~ TYPE",data = type.wall.distance)
 
 newdata <- data.frame(predict(dummy,newdata = type.wall.distance))
 
 type.wall.distance <- cbind(type.wall.distance,newdata)
+
+sample.index <- sample(1:nrow(type.wall.distance)
+                       ,nrow(type.wall.distance)*0.7
+                       ,replace = F)
+predictors <- c("INFRARED","ULTRASONIC","TYPE.CONCAVA","TYPE.CONVEXA","TYPE.PLANA")
+
+
+train.data  <-  type.wall.distance[sample.index
+                                   ,c(predictors,"TYPE")
+                                   ,drop=F]
+test.data  <-  type.wall.distance[-sample.index
+                                   ,c(predictors,"TYPE")
+                                   ,drop=F]
+##KNN
+ctrl <- trainControl(method = "cv",p=0.7) #variable de control
+Knnfit <- train(TYPE ~ INFRARED+ULTRASONIC+TYPE.CONCAVA+TYPE.CONVEXA+TYPE.PLANA
+                ,data = train.data
+                ,method = "knn", trControl = ctrl
+                ,preProcess= c("range")
+                ,tuneLength=20)
+
+Knnpredict <- predict(Knnfit,newdata = test.data)
+
+confusionMatrix(Knnpredict
+                ,test.data$TYPE)
+library(pROC)
+kNN.roc <- roc(test.data$TYPE, as.numeric(as.character(Knnpredict)))
+
 
 
 
